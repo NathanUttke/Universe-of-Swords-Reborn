c53b66228ed715da3bbc36d70e66245c6ce8cace
msb165 <75228465+msb165@users.noreply.github.com>
2023-03-09 14:50:14 -0300

    Clean up and fix Human Buzz's code
    Fixed a problem that made possibly to hit an enemy at enemy distance and also made some alterations to the projectile code. I also made some miscellaneous enhancements like making the player hold the weapon.

--- Projectiles/HumanBuzzSaw.cs
+++ Projectiles/HumanBuzzSaw.cs
@@ -2,7 +2,9 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.Audio;
 using Terraria.GameContent;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace UniverseOfSwordsMod.Projectiles;
@@ -19,51 +21,51 @@ public class HumanBuzzSaw : ModProjectile
         Projectile.width = 30;
         Projectile.height = 30;
         Projectile.scale = 4f;
-        Projectile.friendly = true;
         Projectile.penetrate = -1;
         Projectile.tileCollide = false;
+        Projectile.DamageType = DamageClass.MeleeNoSpeed;
+
         Projectile.ignoreWater = true;
-        Projectile.DamageType = DamageClass.Melee;
+        Projectile.friendly = true;
+        Projectile.ownerHitCheck = true;
+        Projectile.hide = true;
+
+        Projectile.usesLocalNPCImmunity = true;
+        Projectile.localNPCHitCooldown = 15;
+
+        Projectile.aiStyle = -1;
     }
 
     public override void AI()
     {
-        Projectile projectile = Projectile;
-        projectile.soundDelay--;
+        Player player = Main.player[Projectile.owner];
+
+        player.heldProj = Projectile.whoAmI;
+        player.itemTime = player.itemAnimation = 2;
+        player.itemRotation = Projectile.rotation;
+
         if (Projectile.soundDelay <= 0)
         {
-            Projectile.soundDelay = 45;
+            Projectile.soundDelay = 30;
+            SoundEngine.PlaySound(SoundID.Item22, Projectile.position);
         }
-        Player player = Main.player[Projectile.owner];
-        if (Main.myPlayer == Projectile.owner && (!player.channel || player.noItems || player.CCed))
+        
+        if (Main.myPlayer == Projectile.owner && (!player.channel || !player.controlUseItem  || player.noItems || player.CCed))
         {
             Projectile.Kill();
         }
+
         Projectile.Center = player.MountedCenter;
         Projectile.position.X += player.width / 2 * player.direction;
         Projectile.spriteDirection = player.direction;
-        Projectile projectile2 = Projectile;
-        projectile2.rotation += 0.3f * (float)player.direction;
-        if (Projectile.rotation > (float)Math.PI * 2f)
-        {
-            Projectile projectile3 = Projectile;
-            projectile3.rotation -= (float)Math.PI * 2f;
-        }
-        else if (Projectile.rotation < 0f)
-        {
-            Projectile projectile4 = Projectile;
-            projectile4.rotation += (float)Math.PI * 2f;
-        }
-        player.heldProj = Projectile.whoAmI;
-        player.itemTime = 2;
-        player.itemAnimation = 2;
-        player.itemRotation = Projectile.rotation;
+
+        Projectile.rotation += 0.3f * player.direction;
     }
 
     public override bool PreDraw(ref Color lightColor)
     {
         Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-        Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, (Rectangle?)null, Color.White, Projectile.rotation, new Vector2((float)(texture.Width / 2), (float)(texture.Height / 2)), 1f, (SpriteEffects)(Projectile.spriteDirection /*!*/= -1), 0f);
+        Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, new Vector2((float)(texture.Width / 2), (float)(texture.Height / 2)), 1f, (SpriteEffects)(Projectile.spriteDirection /*!*/= -1), 0f);
         return false;
     }
 }
