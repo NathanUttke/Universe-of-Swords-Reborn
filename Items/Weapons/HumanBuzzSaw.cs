e8ea306662914b8ef382d5414f8615b164e000be
msb165 <75228465+msb165@users.noreply.github.com>
2023-03-09 14:52:00 -0300

    Update HumanBuzzSaw.cs

--- Items/Weapons/HumanBuzzSaw.cs
+++ Items/Weapons/HumanBuzzSaw.cs
@@ -1,71 +1,67 @@
-using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Terraria;
-using Terraria.Audio;
-using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace UniverseOfSwordsMod.Projectiles;
+namespace UniverseOfSwordsMod.Items.Weapons;
 
-public class HumanBuzzSaw : ModProjectile
+public class HumanBuzzSaw : ModItem
 {
-    public override void SetStaticDefaults()
-    {
-        DisplayName.SetDefault("Human Buzz Saw");
+	public override void SetStaticDefaults()
+	{
+		DisplayName.SetDefault("Human Buzz Saw");
+		Tooltip.SetDefault("'Cuts through hordes of Terraria like butter'");
+	}
+
+	public override void SetDefaults()
+	{
+        SacrificeTotal = 1;
+
+        Item.damage = 24;
+		Item.DamageType = DamageClass.MeleeNoSpeed; 
+
+		Item.width = 106;
+		Item.height = 106;
+		Item.crit = 8;
+		Item.scale = 2f;
+
+		Item.useStyle = ItemUseStyleID.Shoot;
+
+		Item.useTime = 4;
+		Item.useAnimation = 4;		
+		Item.UseSound = SoundID.Item1;
+
+		Item.knockBack = 5f;
+		Item.value = Item.sellPrice(0, 5, 0, 0);
+		Item.rare = ItemRarityID.LightRed;
+		Item.shoot = Mod.Find<ModProjectile>("HumanBuzzSaw").Type;
+
+        Item.channel = true;
+        Item.autoReuse = true;
+        Item.noUseGraphic = true;
+        Item.noMelee = true;
     }
 
-    public override void SetDefaults()
-    {
-        Projectile.width = 30;
-        Projectile.height = 30;
-        Projectile.scale = 4f;
-        Projectile.penetrate = -1;
-        Projectile.tileCollide = false;
-        Projectile.DamageType = DamageClass.MeleeNoSpeed;
+	//public override bool UseItemFrame(Player player)
+	//{
+	//	player.bodyFrame.Y = 3 * player.bodyFrame.Height;
+	//	return true;
+	//}
 
-        Projectile.ignoreWater = true;
-        Projectile.friendly = true;
-        Projectile.ownerHitCheck = true;
-        Projectile.hide = true;
-
-        Projectile.usesLocalNPCImmunity = true;
-        Projectile.localNPCHitCooldown = 15;
-
-        Projectile.aiStyle = -1;
-    }
-
-    public override void AI()
-    {
-        Player player = Main.player[Projectile.owner];
-
-        player.heldProj = Projectile.whoAmI;
-        player.itemTime = player.itemAnimation = 2;
-        player.itemRotation = Projectile.rotation;
-
-        if (Projectile.soundDelay <= 0)
-        {
-            Projectile.soundDelay = 30;
-            SoundEngine.PlaySound(SoundID.Item22, Projectile.position);
-        }
-        
-        if (Main.myPlayer == Projectile.owner && (!player.channel || !player.controlUseItem  || player.noItems || player.CCed))
-        {
-            Projectile.Kill();
-        }
-
-        Projectile.Center = player.MountedCenter;
-        Projectile.position.X += player.width / 2 * player.direction;
-        Projectile.spriteDirection = player.direction;
-
-        Projectile.rotation += 0.3f * player.direction;
-    }
-
-    public override bool PreDraw(ref Color lightColor)
-    {
-        Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-        Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, new Vector2((float)(texture.Width / 2), (float)(texture.Height / 2)), 1f, (SpriteEffects)(Projectile.spriteDirection /*!*/= -1), 0f);
-        return false;
-    }
+	public override void AddRecipes()
+	{
+		Recipe val = CreateRecipe(1);
+		val.AddIngredient(ItemID.Sawmill, 1);
+		val.AddIngredient(ItemID.TitaniumBar, 8);
+		val.AddIngredient(Mod, "SwordMatter", 60);
+		val.AddIngredient(Mod, "DamascusBar", 20);
+		val.AddTile(TileID.MythrilAnvil);
+		val.Register();
+		Recipe val2 = CreateRecipe(1);
+		val2.AddIngredient(ItemID.Sawmill, 1);
+		val2.AddIngredient(ItemID.AdamantiteBar, 8);
+		val2.AddIngredient(Mod, "SwordMatter", 60);
+		val2.AddIngredient(Mod, "DamascusBar", 20);
+		val2.AddTile(TileID.MythrilAnvil);
+		val2.Register();
+	}
 }
