7eebe9ee3418a0ed19c8c39ec653772fe482aff2
msb165 <75228465+msb165@users.noreply.github.com>
2023-03-15 20:04:48 -0300

    Update HaloOfHorrors.cs

--- Items/HaloOfHorrors.cs
+++ Items/HaloOfHorrors.cs
@@ -9,7 +9,7 @@ public class HaloOfHorrors : ModItem
 	public override void SetStaticDefaults()
 	{
 		DisplayName.SetDefault("Skull of Insanity");
-		Tooltip.SetDefault("'The fearsome power of the Dungeon Guardian is now yours!'\n100 defense\n100% increased armor penetration\nMaximum life increased by 2000\nProvides ultimate health regeneration\n15% increased damage\nCurses the wearer with infinite Potion Sickness debuff\n30% decreased melee speed");
+		Tooltip.SetDefault("'The fearsome power of the Dungeon Guardian is now yours!'\n20 defense\n100% increased armor penetration\nIncreased health regeneration\nProvides ultimate health regeneration\n15% increased damage\nCurses the wearer with infinite Potion Sickness debuff\n30% decreased melee speed");
 	}
 
 	public override void SetDefaults()
@@ -25,11 +25,11 @@ public class HaloOfHorrors : ModItem
 
 	public override void UpdateAccessory(Player player, bool hideVisual)
 	{
-		player.statDefense += 80;
-		player.GetArmorPenetration(DamageClass.Generic) += 100;
+		player.statDefense += 20;
+		player.GetArmorPenetration(DamageClass.Generic) += 25;
 		player.lifeRegen += 35;
 		player.GetAttackSpeed(DamageClass.Melee) -= 0.25f;
-		player.statLifeMax2 += 500;
+		player.statLifeMax2 += 100;
 		player.GetDamage(DamageClass.Generic) += 0.15f;
 		player.AddBuff(21, 2, true);
 	}
