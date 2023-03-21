72eac3746b920c0590790b179a7a4e67b278d16e
msb165 <75228465+msb165@users.noreply.github.com>
2023-03-21 17:57:21 -0300

    Update sprites, remove "Star Maelstorm"

--- Items/Weapons/GreatswordOfTheCosmos.cs
+++ Items/Weapons/GreatswordOfTheCosmos.cs
@@ -90,20 +90,5 @@ public class GreatswordOfTheCosmos : ModItem
 			.AddIngredient(ModContent.ItemType<UpgradeMatter>(), 10)
 			.AddTile(TileID.LunarCraftingStation)
 			.Register();
-        CreateRecipe()
-			.AddIngredient(ModContent.ItemType<StarMaelstorm>(), 1)
-			.AddIngredient(Mod, "Saphira", 1)
-			.AddIngredient(ItemID.FragmentSolar, 30)
-			.AddIngredient(ItemID.FragmentVortex, 30)
-			.AddIngredient(ItemID.FragmentNebula, 30)
-			.AddIngredient(ItemID.FragmentStardust, 30)
-			.AddIngredient(ModContent.ItemType<PowerOfTheGalactic>(), 1)
-			.AddIngredient(ItemID.MeteoriteBar, 100)
-			.AddIngredient(ItemID.HellstoneBar, 100)
-			.AddIngredient(ModContent.ItemType<Orichalcon>(), 10)
-			.AddIngredient(ItemID.LunarBar, 50)
-			.AddIngredient(ModContent.ItemType<UpgradeMatter>(), 10)
-			.AddTile(TileID.LunarCraftingStation)
-			.Register();
     }
 }
