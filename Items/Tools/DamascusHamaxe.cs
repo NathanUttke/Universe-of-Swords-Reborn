6da7e9faaf3cbfe8e1e945ebfb306600a22d7186
msb165 <75228465+msb165@users.noreply.github.com>
2023-03-24 00:07:24 -0300

    Update DamascusHamaxe.cs

--- Items/Tools/DamascusHamaxe.cs
+++ Items/Tools/DamascusHamaxe.cs
@@ -2,7 +2,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace UniverseOfSwordsMod.Items;
+namespace UniverseOfSwordsMod.Items.Tools;
 
 public class DamascusHamaxe : ModItem
 {
