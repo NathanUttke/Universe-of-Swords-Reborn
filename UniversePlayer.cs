43d33907d76cc9967bea9a875703973d9f7fc0d8
msb165 <75228465+msb165@users.noreply.github.com>
2023-05-09 17:06:44 -0300

    Update UniversePlayer.cs

--- UniversePlayer.cs
+++ UniversePlayer.cs
@@ -2,6 +2,7 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ModLoader;
+using UniverseOfSwordsMod.Dusts;
 
 namespace UniverseOfSwordsMod;
 
@@ -17,32 +18,23 @@ public class UniversePlayer : ModPlayer
 	public override void UpdateBadLifeRegen()
 	{
 		if (eBlaze)
-		{
-			Player player = ((ModPlayer)this).Player;
-			player.lifeRegen -= 50;
+		{			
+			Player.lifeRegen -= 75;
 		}
 	}
-	public override void PostUpdateMiscEffects()
-	{
-		//if (QuiGin == true)
-		{
-
-		}
-	}
-
+	
 	public override void DrawEffects(PlayerDrawSet drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
 	{
 		if (eBlaze)
 		{
-			if (Main.rand.Next(8) == 0 && drawInfo.shadow == 0f)
+			if (Main.rand.NextBool(8) && drawInfo.shadow == 0f)
 			{
-				int dust = Dust.NewDust(drawInfo.Position - new Vector2(2f, 2f), ((Entity)((ModPlayer)this).Player).width + 4, ((Entity)((ModPlayer)this).Player).height + 4, ((ModPlayer)this).Mod.Find<ModDust>("EmperorBlaze").Type, ((Entity)((ModPlayer)this).Player).velocity.X * 0.4f, ((Entity)((ModPlayer)this).Player).velocity.Y * 0.4f, 100, default(Color), 3f);
+				int dust = Dust.NewDust(drawInfo.Position - new Vector2(2f, 2f), Player.width + 4, Player.height + 4, ModContent.DustType<EmperorBlaze>(), Player.velocity.X * 0.4f, Player.velocity.Y * 0.4f, 100, default(Color), 3f);
 				Main.dust[dust].noGravity = true;
 				Dust obj = Main.dust[dust];
 				obj.velocity *= 0.8f;
-				Main.dust[dust].velocity.Y -= 0.5f;
-				//Main.playerDrawDust.Add(dust);
-				Main.dust[(int)dust].noGravity = false;	
+				Main.dust[dust].velocity.Y -= 0.5f;				
+				Main.dust[dust].noGravity = false;	
 			}
 			r *= 1f;
 			g *= 0.5f;
