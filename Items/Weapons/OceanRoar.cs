3e3c002d569d1d1120631bc74d4f03cbcb84f3a9
msb165 <75228465+msb165@users.noreply.github.com>
2023-05-08 23:20:08 -0300

    Update OceanRoar.cs

--- Items/Weapons/OceanRoar.cs
+++ Items/Weapons/OceanRoar.cs
@@ -11,11 +11,11 @@ public class OceanRoar : ModItem
 	{
 		Item.width = 32;
 		Item.height = 32;
-		Item.scale = 0.9f;
+		Item.scale = 1f;
 		Item.rare = ItemRarityID.Orange;
 		Item.useStyle = ItemUseStyleID.Swing;
 		Item.useTime = 50;
-		Item.useAnimation = 50;
+		Item.useAnimation = 25;
 		Item.damage = 10;
 		Item.shoot = ProjectileID.Typhoon;
 		Item.shootSpeed = 1f;
