8cccaa95b16ef0985778839d100f54c77e89202d
msb165 <75228465+msb165@users.noreply.github.com>
2023-05-08 23:20:10 -0300

    Update SapphireSword.cs

--- Items/Weapons/SapphireSword.cs
+++ Items/Weapons/SapphireSword.cs
@@ -13,24 +13,22 @@ public class SapphireSword : ModItem
 		Item.height = 44;
 		Item.scale = 1f;
 		Item.rare = ItemRarityID.Blue;
+
 		Item.useStyle = ItemUseStyleID.Swing;
 		Item.useTime = 30;
 		Item.useAnimation = 30;
-		Item.damage = 15;
-		Item.knockBack = 3f;
-		Item.UseSound = SoundID.Item1;
+        Item.UseSound = SoundID.Item1;
+
+        Item.damage = 15;
+		Item.knockBack = 3f;		
 		Item.value = Item.sellPrice(0, 0, 20, 0);
 		Item.autoReuse = true;
-		Item.DamageType = DamageClass.Melee; SacrificeTotal = 1;
+		Item.DamageType = DamageClass.Melee; 
+		SacrificeTotal = 1;
 	}
 
 	public override void MeleeEffects(Player player, Rectangle hitbox)
-	{
-		
-		
-		
-		
-					
+	{					
 		if (Main.rand.NextBool(2))
 		{
 			int dust = Dust.NewDust(new Vector2((float)hitbox.X, (float)hitbox.Y), hitbox.Width, hitbox.Height, DustID.BlueTorch, 0f, 0f, 100, default(Color), 2f);
