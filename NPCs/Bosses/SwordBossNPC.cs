0d0fbf38944384921a7b31f4e30b83c1a84423ba
msb165 <75228465+msb165@users.noreply.github.com>
2023-06-24 11:54:11 -0300

    Update SwordBossNPC.cs

--- NPCs/Bosses/SwordBossNPC.cs
+++ NPCs/Bosses/SwordBossNPC.cs
@@ -5,6 +5,7 @@ using System;
 using Terraria;
 using Terraria.Audio;
 using Terraria.DataStructures;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.PlayerDrawLayer;
@@ -19,6 +20,9 @@ namespace UniverseOfSwordsMod.NPCs.Bosses
         {
             DisplayName.SetDefault("Cursed Sword");
             Main.npcFrameCount[Type] = 1;
+            NPCID.Sets.TrailCacheLength[Type] = 15;
+            NPCID.Sets.TrailingMode[Type] = 3;
+
 
             NPCID.Sets.MPAllowedEnemies[Type] = true;
             // Automatically group with other bosses
@@ -48,7 +52,6 @@ namespace UniverseOfSwordsMod.NPCs.Bosses
             NPC.knockBackResist = 0f;
             NPC.HitSound = SoundID.NPCHit4;
             NPC.DeathSound = SoundID.NPCDeath6;
-            //NPC.SpawnWithHigherTime(30);
             NPC.npcSlots = 10f;
             NPC.aiStyle = -1;
             NPC.value = Item.buyPrice(0, 5, 0, 0);
@@ -57,7 +60,7 @@ namespace UniverseOfSwordsMod.NPCs.Bosses
 
         public override bool? DrawHealthBar(byte hbPosition, ref float scale, ref Vector2 position)
         {
-            return NPC.dontTakeDamage;
+            return null;
         }
 
         public override bool CanHitPlayer(Player target, ref int cooldownSlot)
@@ -80,6 +83,7 @@ namespace UniverseOfSwordsMod.NPCs.Bosses
 
         private float dashSpeed = 18f;
         private Player player => Main.player[NPC.target];
+        private float timeToChange = 15f;
         public override void AI()
         { 
             base.AI();
@@ -121,10 +125,10 @@ namespace UniverseOfSwordsMod.NPCs.Bosses
                     float playerPosNpcSqrt = 5f / playerPosNpc.Length();
                     playerPosNpc *= playerPosNpcSqrt;
                     npcPosition += playerPosNpc;
-                    if (Main.rand.NextBool(2))
+                    if (Main.rand.NextBool(5))
                     {
                         int enchantSword = NPC.NewNPC(NPC.GetSource_FromAI(), (int)npcPosition.X, (int)npcPosition.Y, NPCID.EnchantedSword);
-                        if (Main.netMode == NetmodeID.Server && enchantSword < 20)
+                        if (Main.netMode == NetmodeID.Server && enchantSword < 10)
                         {
                             NetMessage.SendData(MessageID.SyncNPC, -1, -1, null, enchantSword);
                         }
@@ -142,42 +146,36 @@ namespace UniverseOfSwordsMod.NPCs.Bosses
             }
             else if (NPC.ai[0] == 2f)
             {
+                if (NPC.life <= NPC.lifeMax * 0.5)
+                {
+                    timeToChange = 8f;
+                }
+
                 Vector2 npcPosition = new(NPC.position.X + NPC.width * 0.5f, NPC.position.Y + NPC.height * 0.5f);
                 Vector2 playerPosNpc = new(player.position.X + player.width / 2 - npcPosition.X, player.position.Y + player.height / 2 - npcPosition.Y);
-                float playerPosNpcSqrt = 5f / playerPosNpc.Length();
+                float playerPosNpcSqrt = (dashSpeed * 0.75f) / playerPosNpc.Length();
                 playerPosNpc *= playerPosNpcSqrt;
                 npcPosition += playerPosNpc;
 
                 NPC.velocity *= 0.99f;
                 NPC.ai[1] += 1f;
+                NPC.rotation += 0.25f;
 
-                /*if (Math.Sign(NPC.velocity.X) != 0)
+                if (NPC.ai[1] >= timeToChange)
                 {
-                    NPC.spriteDirection = -Math.Sign(NPC.velocity.X);
-                }*/
-
-                if (NPC.ai[1] >= 15f)
-                {
-
-                    if (NPC.life <= NPC.lifeMax * 0.75)
+                    if (NPC.life <= NPC.lifeMax * 0.75 && Main.netMode != NetmodeID.MultiplayerClient)
                     {
-                        int i = 0;
-                        while (i < 3)
+                        for (int i = 0; i < 10; i++)
                         {
                             Vector2 spinPoint = Vector2.Normalize(playerPosNpc * 4f) * 14f;
-                            int rotationRadius = i == 2 ? -15 : 15 * i;
-                            spinPoint = spinPoint.RotatedBy(MathHelper.ToRadians(rotationRadius));
-                            if (spinPoint.HasNaNs())
-                            {
-                                spinPoint -= Vector2.UnitY;
-                            }
+                            spinPoint = spinPoint.RotatedBy(-i * MathHelper.Pi / 5f, Vector2.Zero);
                             Projectile swordProj = Projectile.NewProjectileDirect(NPC.GetSource_FromAI(), npcPosition, spinPoint, ProjectileID.LightBeam, 15, 0f, player.whoAmI);
                             swordProj.friendly = false;
                             swordProj.hostile = true;
-                            i++;
                         }
                     }
 
+                    SoundEngine.PlaySound(SoundID.Item71, NPC.position);
                     NPC.netUpdate = true;
                     NPC.ai[0] = 0f;
                     NPC.ai[1] = 0f;
@@ -186,7 +184,20 @@ namespace UniverseOfSwordsMod.NPCs.Bosses
         }
         public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
         {
-            return base.PreDraw(spriteBatch, screenPos, drawColor);
+            Vector2 vector12 = new(TextureAssets.Npc[Type].Width() / 2, TextureAssets.Npc[Type].Height() / Main.npcFrameCount[Type] / 2);
+            Color npcColor = Lighting.GetColor((int)(NPC.position.X + NPC.width * 0.5) / 16, (int)((NPC.position.Y + NPC.height * 0.5) / 16.0));
+            float newScale = NPC.scale;
+            for (int i = 1; i < NPC.oldPos.Length; i++)
+            {                
+                newScale -= 0.1f;
+                Color newColor = npcColor;
+                newColor.R /= 2;
+                newColor.G /= 2;
+                newColor.B /= 2;
+                newColor.A /= 2;
+                spriteBatch.Draw(TextureAssets.Npc[Type].Value, new Vector2(NPC.oldPos[i].X - Main.screenPosition.X + NPC.width / 2 - TextureAssets.Npc[Type].Width() * NPC.scale / 2f + vector12.X * NPC.scale, NPC.oldPos[i].Y - Main.screenPosition.Y + NPC.height - TextureAssets.Npc[Type].Height() * NPC.scale / Main.npcFrameCount[Type] + 4f + vector12.Y * NPC.scale), NPC.frame, newColor, NPC.rotation, vector12, newScale, SpriteEffects.None, 0f);
+            }
+            return true;
         }
     }
 }
