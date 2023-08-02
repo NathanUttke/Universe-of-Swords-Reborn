340caebd7916171c26ef125aeda754db1e721626
msb165 <75228465+msb165@users.noreply.github.com>
2023-08-02 12:53:27 -0300

    Update UniverseOfSwordsMod.cs

--- UniverseOfSwordsMod.cs
+++ UniverseOfSwordsMod.cs
@@ -1,17 +1,19 @@
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
 using Terraria.ModLoader;
+using UniverseOfSwordsMod.Common.Systems;
+using UniverseOfSwordsMod.Items.Consumables;
+using UniverseOfSwordsMod.NPCs.Bosses;
 
-namespace UniverseOfSwordsMod;
-
-public class UniverseOfSwordsMod : Mod
+namespace UniverseOfSwordsMod
 {
-	public UniverseOfSwordsMod()
-	{
-
-		//((Mod)this).Properties/* tModPorter Note: Removed. Instead, assign the properties directly (ContentAutoloadingEnabled, GoreAutoloadingEnabled, MusicAutoloadingEnabled, and BackgroundAutoloadingEnabled) */ = new ModProperties
-		//{
-		//	Autoload = true,
-		//	AutoloadGores = true,
-		//	AutoloadSounds = true
-		//};
-	}
+    public class UniverseOfSwordsMod : Mod
+    {
+        public override void Load()
+        {
+            base.Load();
+        }
+    }
 }
+
