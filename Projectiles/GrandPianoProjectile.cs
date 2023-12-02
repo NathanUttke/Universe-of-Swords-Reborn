612b3a002da9e688a3228b961d8f42b5e60b12b9
msb165 <75228465+msb165@users.noreply.github.com>
2023-12-02 00:42:03 -0300

    Update GrandPianoProjectile.cs

--- Projectiles/GrandPianoProjectile.cs
+++ Projectiles/GrandPianoProjectile.cs
@@ -1,4 +1,7 @@
-﻿using Terraria.ID;
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace UniverseOfSwordsMod.Projectiles
@@ -13,8 +16,81 @@ namespace UniverseOfSwordsMod.Projectiles
             Projectile.penetrate = -1;
             Projectile.tileCollide = false;
             Projectile.light = 0.33f;
-            Projectile.aiStyle = ProjAIStyleID.Boomerang;
-            AIType = ProjectileID.PaladinsHammerFriendly;
+        }
+
+        public override void AI()
+        {
+            if (Projectile.ai[0] == 0f)
+            {
+                Projectile.ai[1] += 1f;
+                if (Projectile.ai[1] >= 30f)
+                {
+                    Projectile.ai[0] = 1f;
+                    Projectile.ai[1] = 0f;
+                    Projectile.netUpdate = true;
+                }
+            }
+            else
+            {
+                float acceleration = 3f;
+                Vector2 vector6 = new(Projectile.position.X + Projectile.width * 0.5f, Projectile.position.Y + Projectile.height * 0.5f);
+                float playerWidthPosX = Main.player[Projectile.owner].position.X + (Main.player[Projectile.owner].width / 2) - vector6.X;
+                float playerHeightPosY = Main.player[Projectile.owner].position.Y + (Main.player[Projectile.owner].height / 2) - vector6.Y;
+                float num64 = (float)Math.Sqrt(playerWidthPosX * playerWidthPosX + playerHeightPosY * playerHeightPosY);
+                if (num64 > 3000f)
+                {
+                    Projectile.Kill();
+                }
+                num64 = 15f / num64;
+                playerWidthPosX *= num64;
+                playerHeightPosY *= num64;
+
+                if (Projectile.velocity.X < playerWidthPosX)
+                {
+                    Projectile.velocity.X += acceleration;
+                    if (Projectile.velocity.X < 0f && playerWidthPosX > 0f)
+                    {
+                        Projectile.velocity.X += acceleration;
+                    }
+                }
+                else if (Projectile.velocity.X > playerWidthPosX)
+                {
+                    Projectile.velocity.X -= acceleration;
+                    if (Projectile.velocity.X > 0f && playerWidthPosX < 0f)
+                    {
+                        Projectile.velocity.X -= acceleration;
+                    }
+                }
+                if (Projectile.velocity.Y < playerHeightPosY)
+                {
+                    Projectile.velocity.Y += acceleration;
+                    if (Projectile.velocity.Y < 0f && playerHeightPosY > 0f)
+                    {
+                        Projectile.velocity.Y += acceleration;
+                    }
+                }
+                else if (Projectile.velocity.Y > playerHeightPosY)
+                {
+                    Projectile.velocity.Y -= acceleration;
+                    if (Projectile.velocity.Y > 0f && playerHeightPosY < 0f)
+                    {
+                        Projectile.velocity.Y -= acceleration;
+                    }
+                }
+
+                if (Main.myPlayer == Projectile.owner)
+                {
+                    Rectangle rectangle = new((int)Projectile.position.X, (int)Projectile.position.Y, Projectile.width, Projectile.height);
+                    Rectangle value = new((int)Main.player[Projectile.owner].position.X, (int)Main.player[Projectile.owner].position.Y, Main.player[Projectile.owner].width, Main.player[Projectile.owner].height);
+                    if (rectangle.Intersects(value))
+                    {
+                        Projectile.Kill();
+                    }
+                }
+            }
+
+            Lighting.AddLight(Projectile.position, 0.25f, 0.25f, 0.25f);
+            Projectile.rotation += 0.4f * Projectile.direction;
         }
     }
 }
