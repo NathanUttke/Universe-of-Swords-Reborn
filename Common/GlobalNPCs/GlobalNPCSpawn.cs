3ffa06751979529ecc8c9f962688d2f6e9793639
msb165 <75228465+msb165@users.noreply.github.com>
2023-12-17 12:07:27 -0300

    Create GlobalNPCSpawn.cs

---
+++ Common/GlobalNPCs/GlobalNPCSpawn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Utilities;
+
+namespace UniverseOfSwordsMod.Common.GlobalNPCs
+{
+    public class GlobalNPCSpawn : GlobalNPC
+    {
+        public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+        {
+            if (Main.hardMode && spawnInfo.Player.ZoneForest)
+            {
+                pool[NPCID.EnchantedSword] = SpawnCondition.OverworldDay.Chance;
+            }
+        }
+    }
+}
