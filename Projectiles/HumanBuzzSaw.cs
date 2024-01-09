27a0b75e064bc3042510833c841cd95d73acbfba
msb165 <75228465+msb165@users.noreply.github.com>
2024-01-09 10:37:30 -0300

    Update HumanBuzzSaw.cs

--- Projectiles/HumanBuzzSaw.cs
+++ Projectiles/HumanBuzzSaw.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
@@ -12,15 +11,10 @@ namespace UniverseOfSwordsMod.Projectiles;
 public class HumanBuzzSaw : ModProjectile
 {
     public override string Texture => ModContent.GetInstance<Items.Weapons.HumanBuzzSaw>().Texture;
-    public override void SetStaticDefaults()
-    {
-        // DisplayName.SetDefault("Human Buzz Saw");
-    }
 
     public override void SetDefaults()
     {
-        Projectile.width = 102;
-        Projectile.height = 102;
+        Projectile.Size = new(102);
         Projectile.scale = 1f;
         Projectile.penetrate = -1;
         Projectile.tileCollide = false;
@@ -37,36 +31,45 @@ public class HumanBuzzSaw : ModProjectile
         Projectile.aiStyle = -1;
     }
 
+    Player Owner => Main.player[Projectile.owner];
+
     public override void AI()
     {
-        Player player = Main.player[Projectile.owner];
+        if (!Owner.channel || !Owner.controlUseItem || Owner.noItems || Owner.CCed)
+        {
+            Projectile.Kill();
+        }
 
-        player.heldProj = Projectile.whoAmI;
-        player.SetDummyItemTime(2);
-        player.itemTime = player.itemAnimation = 2;
-        player.itemRotation = Projectile.rotation;
+        SetPlayerValues();
 
         if (Projectile.soundDelay <= 0)
         {
             Projectile.soundDelay = 30;
             SoundEngine.PlaySound(SoundID.Item22, Projectile.position);
-        }
-        
-        if (Main.myPlayer == Projectile.owner && (!player.channel || !player.controlUseItem  || player.noItems || player.CCed))
-        {
-            Projectile.Kill();
-        }
+        }       
 
-        Projectile.Center = player.MountedCenter;
-        Projectile.position.X += player.width / 2 * player.direction;
-        Projectile.spriteDirection = player.direction;
+        Vector2 unitVectorTowardsMouse = Owner.MountedCenter.DirectionTo(Main.MouseWorld);
+        Owner.ChangeDir((unitVectorTowardsMouse.X > 0f).ToDirectionInt());
 
-        Projectile.rotation += 0.3f * player.direction;
+        Projectile.Center = Owner.MountedCenter;
+        Projectile.velocity = unitVectorTowardsMouse;
+        Projectile.position.X += Owner.width / 2 * Owner.direction;
+        Projectile.spriteDirection = Owner.direction;
+
+        Projectile.rotation += 0.3f * Owner.direction;
+    }
+
+    private void SetPlayerValues()
+    {
+        Owner.heldProj = Projectile.whoAmI;
+        Owner.SetDummyItemTime(2);
+        Owner.itemTime = Owner.itemAnimation = 2;
+        Owner.itemRotation = Projectile.rotation;
     }
     public override bool PreDraw(ref Color lightColor)
     {
         Texture2D texture = TextureAssets.Projectile[Projectile.type].Value;
-        Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, new Vector2(texture.Width / 2, texture.Height / 2), 1f, SpriteEffects.None, 0f);
+        Main.spriteBatch.Draw(texture, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation, texture.Size() / 2f, Projectile.scale, SpriteEffects.None, 0f);
         return false;
     }
 }
