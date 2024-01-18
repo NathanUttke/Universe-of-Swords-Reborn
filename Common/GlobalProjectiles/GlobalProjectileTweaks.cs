8d1042a3bf7f4e21d9774ae9dc01771241f52d4a
msb165 <75228465+msb165@users.noreply.github.com>
2024-01-18 15:33:37 -0300

    Create GlobalProjectileTweaks.cs

---
+++ Common/GlobalProjectiles/GlobalProjectileTweaks.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace UniverseOfSwordsMod.Common.GlobalProjectiles
+{
+    internal class GlobalProjectileTweaks : GlobalProjectile
+    {
+        public override void SetDefaults(Projectile entity)
+        {
+            if (entity.type == ProjectileID.SwordBeam)
+            {
+                entity.timeLeft = 40;
+            }
+        }
+    }
+}
