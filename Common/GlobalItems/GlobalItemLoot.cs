3ae19e758fd2682973c36f7d082ecf15acb2f23d
msb165 <75228465+msb165@users.noreply.github.com>
2024-01-18 15:33:32 -0300

    Update GlobalItemLoot.cs

--- Common/GlobalItems/GlobalItemLoot.cs
+++ Common/GlobalItems/GlobalItemLoot.cs
@@ -11,10 +11,13 @@ namespace UniverseOfSwordsMod.Common.GlobalItems
     {
         public override void ModifyItemLoot(Item item, ItemLoot itemLoot)
         {            
-            if (ItemID.Sets.IsFishingCrate[item.type] || ItemID.Sets.IsFishingCrateHardmode[item.type])
+            if (ItemID.Sets.IsFishingCrateHardmode[item.type])
             {
                 itemLoot.Add(ItemDropRule.NotScalingWithLuck(ModContent.ItemType<DamascusOre>(), 7, 6, 23));
                 itemLoot.Add(ItemDropRule.NotScalingWithLuck(ModContent.ItemType<DamascusBar>(), 8, 2, 7));
+            } 
+            if (ItemID.Sets.IsFishingCrate[item.type] || ItemID.Sets.IsFishingCrateHardmode[item.type])
+            {
                 itemLoot.Add(ItemDropRule.NotScalingWithLuck(ModContent.ItemType<MeleePowerPotion>(), 7, 1, 3));
                 itemLoot.Add(ItemDropRule.NotScalingWithLuck(ModContent.ItemType<SwordMatter>(), 40, 1, 2));
             }                     
