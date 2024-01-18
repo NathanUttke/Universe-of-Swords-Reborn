1bbb3c6b928af43028fbbebf9b6d053b41ec5386
msb165 <75228465+msb165@users.noreply.github.com>
2024-01-18 15:33:34 -0300

    Update SwordGlobalItem.cs

--- Common/GlobalItems/SwordGlobalItem.cs
+++ Common/GlobalItems/SwordGlobalItem.cs
@@ -1,4 +1,6 @@
-﻿using Terraria;
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -6,9 +8,20 @@ namespace UniverseOfSwordsMod.Common.GlobalItems
 {
     public class SwordGlobalItem : GlobalItem
     {
-        public override bool AppliesToEntity(Item entity, bool lateInstantiation)
+
+        /*public override bool Shoot(Item item, Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            return entity.type == ItemID.ChlorophyteClaymore;
-        }
+            if (item.type == ItemID.OrichalcumSword)
+            {
+                item.noMelee = true;                
+                float adjustedItemScale = player.GetAdjustedItemScale(item); // Get the melee scale of the player and item.
+                Projectile.NewProjectile(source, player.MountedCenter, new Vector2(player.direction, 0f), ProjectileID.TrueExcalibur, damage, knockback, player.whoAmI, player.direction * player.gravDir, player.itemAnimationMax, adjustedItemScale + 0.2f);
+                NetMessage.SendData(MessageID.PlayerControls, -1, -1, null, player.whoAmI); // Sync the changes in multiplayer.
+                return true;
+            }
+
+            return false;
+        }*/
+
     }
 }
