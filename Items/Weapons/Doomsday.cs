5f0f39d4369169bf08ef0f5476b99ab1a34e10ec
msb165 <75228465+msb165@users.noreply.github.com>
2024-03-20 16:15:18 -0300

    Update Doomsday.cs

--- Items/Weapons/Doomsday.cs
+++ Items/Weapons/Doomsday.cs
@@ -4,6 +4,7 @@ using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using UniverseOfSwordsMod.Dusts;
 using UniverseOfSwordsMod.Projectiles;
 
 namespace UniverseOfSwordsMod.Items.Weapons;
@@ -31,21 +32,31 @@ public class Doomsday : ModItem
 		Item.ResearchUnlockCount = 1;
 	}
 
-    /*public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
     {
+        position += Vector2.UnitY * -70f;
         for (int i = 0; i < 3; i++)
 		{
 			Vector2 newVelocity = velocity * Main.rand.NextFloat(0.9f, 1.25f);
 			newVelocity = newVelocity.RotatedByRandom(0.3f);
-            Projectile.NewProjectileDirect(source, position, newVelocity, type, damage, knockback, player.whoAmI);
+            Projectile.NewProjectileDirect(source, position, newVelocity, type, damage / 2, knockback, player.whoAmI);
         }
         return false;
-    }*/
+    }
 
-    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    public override void MeleeEffects(Player player, Rectangle hitbox)
+    {
+        if (Main.rand.NextBool(2))
+        {
+            int dust = Dust.NewDust(new Vector2(hitbox.X, hitbox.Y), hitbox.Width, hitbox.Height, ModContent.DustType<GlowDust>(), 0f, 0f, 0, Color.Orange, 1.25f);
+            Main.dust[dust].noGravity = true;
+        }
+    }
+
+    /*public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
     {
         position += Vector2.UnitY * -70f;
         Projectile.NewProjectile(source, position, velocity, type, (int)(damage * 1.5f), knockback, player.whoAmI);
         return false;
-    }
+    }*/
 }
