f4bda380eab5a03f1754df70a38bd2d375f13082
msb165 <75228465+msb165@users.noreply.github.com>
2024-03-24 00:36:57 -0300

    Update UniverseOfSwordsMod.cs

--- UniverseOfSwordsMod.cs
+++ UniverseOfSwordsMod.cs
@@ -7,15 +7,31 @@ using Terraria.ModLoader;
 namespace UniverseOfSwordsMod
 {
     public class UniverseOfSwordsMod : Mod
-    {        
+    {
+        internal static UniverseOfSwordsMod Instance;
+
+        internal Mod BombusApisBee = null;
+        internal Mod CalamityMod = null;
         public override void Load()
         {
+            Instance = this;
+
+            BombusApisBee = null;
+            ModLoader.TryGetMod("BombusApisBee", out BombusApisBee);
+            CalamityMod = null;
+            ModLoader.TryGetMod("CalamityMod", out CalamityMod);
 
             if (Main.netMode != NetmodeID.Server)
             {
                 Ref<Effect> PowerGalacticShader = new(ModContent.Request<Effect>("UniverseOfSwordsMod/Assets/Effects/PowerOfTheGalacticShader", AssetRequestMode.ImmediateLoad).Value);
                 GameShaders.Misc["GalacticShader"] = new MiscShaderData(PowerGalacticShader, "PowerGalacticPass").UseProjectionMatrix(true);
             }
+        }        
+
+        public override void Unload()
+        {
+            BombusApisBee = null;
+            CalamityMod = null;
         }
     }
 }
